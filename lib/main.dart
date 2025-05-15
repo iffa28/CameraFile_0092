8b62ffa779bf93a0cde35dd5cf4cce00daf46b5e
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-15 21:12:22 +0700

    menambahkan permission dan panggil halaman utama

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:praktikumkamera/full_page.dart';
+import 'package:praktikumkamera/native_camera_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FullPage(),
     );
   }
 }
